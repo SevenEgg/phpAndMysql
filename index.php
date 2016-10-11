d32878e8fc6c19d9cb73d09b1cf26aac11450294
byzhkc3 <byzhkc3@gmail.com>
2016-10-11 12:21:13 +0800

    更换系统环境：mac

--- index.php
+++ index.php
@@ -10,11 +10,12 @@
 
 
 <?php 
-	$host = 'localhost';
+	// phpinfo();
+	$host = '127.0.0.1';
 	$user = 'root';
 	$pass = '';
 
-	$conn = mysql_connect($host,$user,$pass);
+	$conn = mysqli_connect($host,$user,$pass);
 
 	// 判断是否连接成功
 	if($conn){
@@ -24,14 +25,14 @@
 	}
 
 	// 选择数据库
-	$db = mysql_select_db('mysql');
+	$db = mysqli_select_db($conn,'mysql');
 	if ($db) {
 		echo '<br>'.PHP_EOL;
 		echo '选择数据库成功';
 	}
 
 	// 设置编码
-	mysql_query("set names 'utf8'");
+	mysqli_query($conn,"set names 'utf8'");
 	
 	// 执行插入操作语句
 	// $query = mysql_query('insert into user(User) values("yali1990")');
@@ -48,14 +49,14 @@
 
 
 	// 查询语句
-	$res = mysql_query('select * from user limit 1');
+	$res = mysqli_query($conn,'select * from user limit 1');
 	// 获取资源句柄
-	$row = mysql_fetch_array($res);
+	$row = mysqli_fetch_array($res);
 	// 生成json数据
 	echo json_encode($row);
 
 	// 关闭数据库连接
-	$conn_close = mysql_close($conn);
+	$conn_close = mysqli_close($conn);
 	if($conn_close){
 		echo '<br>'.PHP_EOL;
 		echo '成功关闭数据库';
